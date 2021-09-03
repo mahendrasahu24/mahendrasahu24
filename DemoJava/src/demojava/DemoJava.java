832b5a35230100b84b2a20cbf0f0a8460b6b3922
mahendrasahu24 <88919060+mahendrasahu24@users.noreply.github.com>
2021-09-03 14:48:03 +0530

    Add files via upload

---
+++ DemoJava/src/demojava/DemoJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demojava;
+
+/**
+ *
+ * @author CG-DTE
+ */
+public class DemoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Java");
+        System.out.println("This is Teshu and Mahendra");
+    }
+}
+
